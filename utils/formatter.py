f5a69d101f48ae8f19355e0e6b0f138798045313
Petr Habrych <p.habrych@seznam.cz>
2025-07-09 00:37:10 +0200

    Rows in dictionary in row_sorter

--- utils/formatter.py
+++ utils/formatter.py
@@ -1,4 +1,6 @@
 # this set of functions will format the text from cells and send them to the sticker labels
+from pickle import EMPTY_LIST
+
 import pyperclip as pc
 from constants import auc
 
@@ -24,6 +26,7 @@ def clear_cells(master):
         print("Erase failed")
     return "break"
 
+#This method takes data from columns that are checked and puts them into dict of lists according to column
 def get_checked_cols(checkedboxes, master):
     cells = master.cells
     cells_to_format = {}
@@ -34,6 +37,21 @@ def get_checked_cols(checkedboxes, master):
                 if col not in cells_to_format:
                     cells_to_format[col] = []
                 cells_to_format[col].append(cells[col][cell].get('0.0', "end"))
-                #print(cells[col][cell].get('0.0', "end"))
-    print(cells_to_format)
-    #formattext(cells_to_format)
+    row_sorter(cells_to_format)
+#Let's use this function in the end, when everything is in rows dictionary rather than cols
+def row_sorter(cells: list|dict):
+    print(cells)
+    rows = {}
+    for i, col in enumerate(cells):
+        cell_text = cells[col][i]
+        print(f"i = {i}, col = {col}")
+        print(cell_text)
+        for j, item in enumerate(cells[col]):
+            if j not in rows:
+                text_to_dict = item
+                rows[j] = text_to_dict
+            else:
+                text_to_dict = rows[j] + item
+                rows[j] = text_to_dict
+            #print(f"j = {j}, item = {item}, col = {col}, i = {i}\n text = {rows[j]}")
+    print(rows)
