84dc65c860302299a6e184386c6d8928bd8d7023
Petr Habrych <p.habrych@seznam.cz>
2025-10-10 08:27:51 +0200

    Working updater with config check

--- support/updater.py
+++ support/updater.py
@@ -8,13 +8,14 @@ from pathlib import Path
 
 # Setting up logger to update_log.txt
 update_log_path = Path("support/update_log.txt")
-update_log_file = open(update_log_path, 'w')
+update_log_file = open(update_log_path, "w")
 sys.stdout = update_log_file
 
 # Sys argv with link to the source of new version.
 # Passed from the main app
 SOURCE_URL = sys.argv[1]
-#APP_URL = sys.argv[2]
+# APP_URL = sys.argv[2]
+
 
 # This method is checking the source url
 def check_source(source_url):
@@ -38,15 +39,16 @@ def check_source(source_url):
     except requests.exceptions.RequestException as e:
         print(f"Something went wrong: {e}")
 
-    #Fetching download url
+    # Fetching download url
     print(f"Getting download url")
     try:
-        download_url = response[0]['assets'][0]["browser_download_url"]
+        download_url = response[0]["assets"][0]["browser_download_url"]
         print(f"Download url: {download_url}")
     except (KeyError, IndexError, TypeError) as e:
         print(f"Failed to load the download link: {e}")
     else:
         download_update(download_url)
+
 
 # This function will download the zipfile from the target URL
 def download_update(url):
@@ -64,6 +66,7 @@ def download_update(url):
     parent = Path()
     print(f"{parent.resolve()}")
     deploy_files(response)
+
 
 # This function is managing file extraction and replacement
 def deploy_files(zip_file):
@@ -105,7 +108,50 @@ def deploy_files(zip_file):
         print(f"Permission error: {e}")
     else:
         print("Update complete")
-        start_app(safe_path)
+        #start_app(safe_path)
+        config_check(safe_path)
+
+
+# This function is checking if there is a new configuration file after update.
+def config_check(path):
+    main_path = path  # Path to main folder
+    print(f"Main path: {main_path}")
+    templates_path = main_path / "templates"  # Path to templates
+    print(f"Templates path: {templates_path}")
+    if templates_path / "config.json" and templates_path / "new_config.json":
+        print(f"Config.json and new_config.json in {templates_path}")
+        # Loading old config data
+        with open(templates_path / "config.json", "r", encoding="utf-8") as oc:
+            old_config = json.load(oc)
+        # Loading new config data
+        with open(templates_path / "new_config.json", "r", encoding="utf-8") as nc:
+            new_config = json.load(nc)
+        # Comparing files and adding new data to old config data
+        for key in new_config:
+            print(f"Loading new_config key: {key}")
+            if key not in old_config:
+                print(f"Adding {key} to old_config")
+                old_config[key] = new_config[key]
+        print(old_config, templates_path)
+        write_config_changes(old_config, templates_path, main_path)
+    else:
+        print(f"Missing file in {templates_path}")
+        start_app(main_path)
+
+
+def write_config_changes(config_data, templates, main_path):
+    """
+
+    :param config_data: New data for old config
+    :return: rewrites the config.json file
+    :param templates: Templates folder with config.json
+    """
+    with open(templates / "config.json", "w", encoding="utf-8") as c:
+        print(f"Dumping data to {templates}")
+        json.dump(config_data, c, indent=2)
+
+    start_app(main_path)
+
 
 def start_app(path):
     """
@@ -118,7 +164,7 @@ def start_app(path):
     if platform.system() == "Windows":
         if getattr(sys, "frozen", False):
             print("Running the exprint2.exe file")
-            subprocess.Popen(str(path/"exprint2.exe"))
+            subprocess.Popen(str(path / "exprint2.exe"))
         else:
             print("Running the python file")
             subprocess.Popen(["python3", "main.py"])
@@ -126,40 +172,6 @@ def start_app(path):
         print("Running non windows python file")
         subprocess.Popen(["python3", "main.py"])
 
-def config_check():
-    main_path = Path().resolve() # Path to main folder
-    print(f"Main path: {main_path}")
-    templates_path = main_path/"templates" # Path to templates
-    print(f"Templates path: {templates_path}")
-    if templates_path/"config.json" and templates_path/"new_config.json":
-        print(f"Config.json and new_config.json in {templates_path}")
-        #Loading old config data
-        with open(templates_path/"config.json", 'r', encoding='utf-8') as oc:
-            old_config = json.load(oc)
-        # Loading new config data
-        with open(templates_path/"new_config.json", 'r', encoding='utf-8') as nc:
-            new_config = json.load(nc)
-        # Comparing files and adding new data to old config data
-        for key in new_config:
-            print(f"Loading new_config key: {key}")
-            if key not in old_config:
-                print(f"Adding {key} to old_config")
-                old_config[key] = new_config[key]
-        print(old_config, templates_path)
-        write_config_changes(old_config,templates_path)
-
-def write_config_changes(config_data, templates):
-    """
-
-    :param config_data: New data for old config
-    :return: rewrites the config.json file
-    :param templates: Templates folder with config.json
-    """
-    with open(templates/"config.json", 'w', encoding='utf-8') as c:
-        print(f"Dumping data to {templates}")
-        json.dump(config_data,c,indent=2)
-
 
 if __name__ == "__main__":
-    #check_source(SOURCE_URL)
-    config_check()
+    check_source(SOURCE_URL)
